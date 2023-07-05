8886ab2184b39a583c1a5bbfadca86dded1173fc
Chia Kang Jin <xiankai@gmail.com>
2023-07-06 01:45:23 +0900

    dont need uvicorn it seems

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source venv/bin/activate
 pip install -r requirements.txt
-pkill uvicorn gunicorn
-nohup gunicorn --workers 4 --bind http://localhost:80 app.main:app &
+pkill gunicorn
+nohup gunicorn --workers 4 --bind 127.0.0.1:80 app.main:app &
