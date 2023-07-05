50b8e198da5c2d4da92c9b4255062794c16cce9e
Chia Kang Jin <xiankai@gmail.com>
2023-07-06 01:42:14 +0900

    go back to guvicorn xd

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source venv/bin/activate
 pip install -r requirements.txt
-pkill supervisord uvicorn
-supervisord -n -c deploy/supervisord.conf
+pkill uvicorn gunicorn
+nohup gunicorn --workers 4 --bind http://localhost:80 app.main:app &
