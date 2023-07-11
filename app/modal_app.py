56965056138619966d241ef58f5cecd329a2def9
Chia Kang Jin <xiankai@gmail.com>
2023-07-11 20:41:30 +0900

    modal is smooth

---
+++ app/modal_app.py
@@ -0,0 +1,15 @@
+from app.main import app
+from modal import Image, Stub, asgi_app
+stub = Stub()
+image = Image.debian_slim().pip_install('txtai')
+
+from modal import NetworkFileSystem
+
+volume = NetworkFileSystem.persisted("model-cache-vol")
+@stub.function(
+  network_file_systems={"/root/txtai_embeddings": volume},
+  image=image,
+)
+@asgi_app()
+def fastapi_app():
+  return app
