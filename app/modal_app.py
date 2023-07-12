aba23136dadc02b3d326031669fc46cb42f4eb4c
Chia Kang Jin <xiankai@gmail.com>
2023-07-12 21:59:56 +0900

    try modal as a single file

--- app/modal_app.py
+++ app/modal_app.py
@@ -1,13 +1,66 @@
+from app.utils import log_sql, format_response
+from app.types import Documents
+import datetime
 from modal import Image, Stub, NetworkFileSystem, asgi_app
 stub = Stub("modal-app")
-image = Image.debian_slim().pip_install('txtai')
-
 volume = NetworkFileSystem.persisted("model-cache-vol")
+
+stub["embeddings_image"] = Image.debian_slim().pip_install('txtai')
+if stub.is_inside(stub["embeddings_image"]):
+  from txtai.embeddings import Embeddings
+
+  embeddings = Embeddings({"path": "sentence-transformers/paraphrase-MiniLM-L3-v2", "content": True})
+  # Running from the root directory
+  embeddings.load(path="/root/txtai_embeddings")
+
+from fastapi import FastAPI
+router = FastAPI()
+
+@router.get("/search")
+async def search(
+  q: str,
+  from_date: datetime.date = "",
+  to_date: datetime.date = "",
+  limit: int = 10,
+  offset: int = 0,
+  sort_by: str = "timestamp_ms",
+  order: str = "desc",
+  recipient: str = "",
+  source: str = "",
+):
+  sql_query = f'SELECT text FROM txtai WHERE similar({q}) LIMIT {limit}'
+
+  if from_date and to_date:
+    sql_query += f' AND date BETWEEN $from_date AND $to_date'
+  elif from_date:
+    sql_query += f' AND date = $from_date'
+  elif to_date:
+    sql_query += f' AND date = $to_date'
+
+  if recipient:
+    sql_query += f' AND recipient = $recipient'
+  if source:
+    sql_query += f' AND source = $source'
+
+  log_sql(sql_query)
+
+  docs = embeddings.search(sql_query)
+  return format_response(docs)
+
+@router.get("/day")
+async def day(date: datetime.date, recipient: str, source: str):
+  docs = embeddings.search(f'SELECT * FROM txtai WHERE date = {date} AND recipient = {recipient} AND source = {source}')
+  return format_response(docs)
+
+
+@router.post("/index")
+async def index(docs: Documents):
+  embeddings.index(docs)
+
 @stub.function(
   network_file_systems={"/root/txtai_embeddings": volume},
-  image=image,
+  image=stub["embeddings_image"]
 )
 @asgi_app()
 def fastapi_app():
-  from app.main import app
-  return app
+  return router
