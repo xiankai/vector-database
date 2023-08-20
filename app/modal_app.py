bdf03ef9d426a5309a014d7c98be119d9bb72a76
Chia Kang Jin <xiankai@gmail.com>
2023-08-20 13:45:13 +0900

    run_function with network_file_systems is deprecated. try without it

--- app/modal_app.py
+++ app/modal_app.py
@@ -8,9 +8,7 @@ mount_point = "/root/shared_volume"
 network_file_systems = {mount_point: volume}
 
 stub["embeddings_image"] = Image.debian_slim() \
-  .pip_install_from_requirements('requirements.txt') \
-  .run_function(initialize_embeddings, secret=Secret.from_name("chat-history"), network_file_systems=network_file_systems) \
-  .run_function(initialize_firebase_admin, secret=Secret.from_name("chat-history"), network_file_systems=network_file_systems)
+  .pip_install_from_requirements('requirements.txt')
 
 @stub.function(
   image=stub["embeddings_image"],
