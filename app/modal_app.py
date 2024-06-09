7ee6c0763f96fa0f467c38b8c071c33ea4940363
KJ <xiankai@gmail.com>
2024-06-09 11:53:48 +0900

    fix future deprecation

--- app/modal_app.py
+++ app/modal_app.py
@@ -2,7 +2,7 @@ from app.main import router
 from app.third_party.txtai import initialize_embeddings
 from app.third_party.firebase_admin import initialize_firebase_admin
 from modal import Image, App, NetworkFileSystem, Secret, asgi_app, is_local
-stub = App("modal-app")
+app = App("modal-app")
 volume = NetworkFileSystem.from_name("model-cache-vol", create_if_missing=True)
 mount_point = "/root/shared_volume"
 network_file_systems = {mount_point: volume}
@@ -10,7 +10,7 @@ network_file_systems = {mount_point: volume}
 embeddings_image = Image.debian_slim() \
   .pip_install_from_requirements('requirements.txt')
 
-@stub.function(
+@app.function(
   image=embeddings_image,
   container_idle_timeout=1200,
   secrets=[Secret.from_name("chat-history")],
